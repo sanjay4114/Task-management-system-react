9bb8d7313316dfd4758b0f21bccb19aab0ba0d16
sanjay4114 <sanjaykalaivani30@gmail.com>
2026-04-27 16:49:16 +0530

    Updated CORS for production frontend

--- backend-spring-boot/src/main/java/com/taskmanagement/config/SecurityConfig.java
+++ backend-spring-boot/src/main/java/com/taskmanagement/config/SecurityConfig.java
@@ -82,7 +82,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList(
+        	    "http://localhost:3000",
+        	    "http://localhost:5173",
+        	    "http://localhost:5174",
+        	    "https://task-management-system-react.onrender.com"
+        	));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
